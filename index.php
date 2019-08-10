5a9616353a6bf8f3fbc846279c80ad7cb19e2a05
Wel <wel80@list.ru>
2019-08-10 20:34:47 +0700

    Сайт заработал!

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php 
-    print("Мой первый сценарий!");
+    $message = "Мой первый сценарий!";
 ?>
+
+<!DOCTYPE HTML>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <title>TEST</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <?php
+            print ($message);
+        ?>
+    </body>
+</html>
